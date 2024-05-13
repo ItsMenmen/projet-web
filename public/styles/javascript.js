f8e2befce0c952fe555dbc4fcb3e33af5c40eb99
ItsMenmen <menantjulien2005@gmail.com>
2024-05-13 12:36:54 +0200

    pour diego le con

--- public/styles/javascript.js
+++ public/styles/javascript.js
@@ -1,17 +1,14 @@
-// Fonction pour effectuer une recherche de films
-async function searchMovies(query) {
-    const apiKey = '049db6c77575ff540a76d80746b13585';
-    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=$
-   {query}`;
-    try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.results; // Renvoie les résultats de la recherche
-    } catch (error) {
-    console.error('Erreur lors de la recherche de films :', error);
-    }
-   }
-   // Exemple d'utilisation de la fonction de recherche
-   const results = await searchMovies('Spider-Man');
-   console.log(results); // Affiche les résultats de la recherche dans la console
-   
+const apiKey = '049db6c77575ff540a76d80746b13585';
+const query = 'Jack Reacher';
+const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`;
+
+// Utilisez fetch ou une autre méthode pour récupérer les données depuis apiUrl
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Traitez les données ici (par exemple, affichez les résultats dans votre interface utilisateur)
+    console.log(data.results);
+  })
+  .catch(error => {
+    console.error('Erreur lors de la récupération des données:', error);
+  });
