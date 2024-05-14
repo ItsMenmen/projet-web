9138e2ae7f680ae18cecf688ab71ad34077ce61f
ItsMenmen <menantjulien2005@gmail.com>
2024-05-14 10:28:44 +0200

    diego beteu

--- public/styles/javascript.js
+++ public/styles/javascript.js
@@ -1,14 +1,23 @@
 const apiKey = '049db6c77575ff540a76d80746b13585';
-const query = 'Jack Reacher';
-const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`;
+let query = ''; 
 
-// Utilisez fetch ou une autre méthode pour récupérer les données depuis apiUrl
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Traitez les données ici (par exemple, affichez les résultats dans votre interface utilisateur)
-    console.log(data.results);
-  })
-  .catch(error => {
-    console.error('Erreur lors de la récupération des données:', error);
-  });
+
+const searchButton = document.querySelector('.bouton');
+
+searchButton.addEventListener('click', function () {
+    // Récupérez la valeur de la barre de recherche
+    const searchQuery = document.querySelector('#site-search').value;
+
+    query = searchQuery;
+
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.results);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+        });
+});
